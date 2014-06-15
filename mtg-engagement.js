b736156be4007b1e7e341dc29b3fee04ac1ab427
LB (Ben) Johnston <lb@doubleampersand.com.au>
2014-06-15 16:06:34 +1000

    Now works with 'rounds'
    also put a few extra helpers in

--- mtg-engagement.js
+++ mtg-engagement.js
@@ -1,31 +1,56 @@
 if (Meteor.isClient) {
   //TEMP DATA
-  UI.registerHelper('matches', function() {
-    return [
-      {
-        playerX: 'LB',
-        playerY: 'Andrew',
-        games: [ 'x', 'y', null],
-      },
-      {
-        playerX: 'LB',
-        playerY: 'Joe',
-        games: ['x','y',null],
-      },
-      {
-        playerX: 'Philip',
-        playerY: 'Poo Face',
-        games: ['y','x','x'],
-      },
-      {
-        playerX: 'Samuel',
-        playerY: 'Artyman',
-        games: ['x','x','x'],
-      }
-    ];
+  var matches =  [
+    {
+      playerX: 'LB',
+      playerY: 'Andrew',
+      round: 1,
+      games: [ 'x', 'y', null],
+    },
+    {
+      playerX: 'LB',
+      playerY: 'Joe',
+      round: 1,
+      games: ['x','y',null],
+    },
+    {
+      playerX: 'Philip',
+      playerY: 'Poo Face',
+      round: 2,
+      games: ['y','x','x'],
+    },
+    {
+      playerX: 'Samuel',
+      playerY: 'Artyman',
+      round: 2,
+      games: ['x','x','x'],
+    }
+  ];
+  UI.registerHelper('rounds', function() {
+    var groupedMatches = _.groupBy( matches, function( match ) {
+      return match.round;
+    });
+
+    var rounds = [];
+    _.each( groupedMatches, function( value, key, list ) {
+      rounds.push({
+        round: key,
+        matches: value,
+      });
+    });
+    return rounds;
   });
 
   //Actual Helpers
+  UI.registerHelper( 'totalMatches', function() {
+    return this.matches.length;
+  });
+
+  UI.registerHelper( 'matchCompleted', function() {
+    return _.every( this.games, function( game ) {
+      return game !== null;
+    });
+  });
   UI.registerHelper( 'matchCompletionPercent', function() {
     var gameTotals = _.countBy( this.games, function(game) {
       if ( game === null ) {
@@ -58,6 +83,13 @@ if (Meteor.isClient) {
     }
     return _.every( this.games, function( i ) { return i === player } );
   });
+
+  UI.registerHelper( 'getOrdinalNumber', function( num ) {
+   var s = ["th","st","nd","rd"]
+   var v = num % 100;
+   return num +(s[(v-20)%10]||s[v]||s[0]);
+  });
+
 }
 
 if (Meteor.isServer) {
